831701bc212cb3afbc70eafd3505473d3f36adfa
dddxhh <huangfei32@huawei.com>
2026-04-15 16:41:58 +0800

    feat: add locale loader

---
+++ entrypoints/sidebar/locales/index.ts
@@ -0,0 +1,15 @@
+import zhCN from './zh-CN';
+import enUS from './en-US';
+
+export const messages = {
+  'zh-CN': zhCN,
+  'en-US': enUS
+};
+
+export type LocaleCode = 'zh-CN' | 'en-US';
+
+export const defaultLocale: LocaleCode = 'zh-CN';
+
+export function getLocaleMessages(locale: LocaleCode) {
+  return messages[locale] || messages[defaultLocale];
+}

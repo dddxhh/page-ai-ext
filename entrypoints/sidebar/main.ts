fad20fe7e837018ae2d316a7337af8f010d1e37d
dddxhh <huangfei32@huawei.com>
2026-04-15 16:44:36 +0800

    feat: configure i18n in main.ts

--- entrypoints/sidebar/main.ts
+++ entrypoints/sidebar/main.ts
@@ -1,13 +1,23 @@
 import { createApp } from 'vue';
 import { createPinia } from 'pinia';
+import { createI18n } from 'vue-i18n';
 import ElementPlus from 'element-plus';
 import 'element-plus/dist/index.css';
 import App from './App.vue';
+import { messages, defaultLocale } from './locales';
 
 const app = createApp(App);
 const pinia = createPinia();
 
+const i18n = createI18n({
+  legacy: false,
+  locale: defaultLocale,
+  fallbackLocale: 'en-US',
+  messages
+});
+
 app.use(pinia);
+app.use(i18n);
 app.use(ElementPlus);
 
 app.mount('#app');
